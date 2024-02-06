5573190f5d67e9f2fc24773510c55de65d0af09f
esraakrkmz <109651527+esraakrkmz@users.noreply.github.com>
2024-02-06 23:35:00 +0300

    içerisinde 'de' bulunduran stringleri kabul eden otomata

---
+++ icerisinde-de-bulunduran-stringleri-kabul-eden-otomata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace abdensonra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //içerisinde de bulunduran stringleri kabul eden otomata(otomatanın alfabesi a,b,c,d,e,f,g,h,i,k)
+            string s1 = "abcdfgh";
+
+            int[,] q = new int[3, 10];
+            int state = 0;
+            q[0, 3] = 1;
+            q[1, 4] = 2;
+           
+            for (int i = 0; i < 10; i++)
+            {
+                q[2, i] = 2;
+
+            }
+           
+            for (int i = 0; i < s1.Length; i++)
+            {
+                state = q[state, s1[i] - 'a'];
+                
+            }
+            if (state == 2) Console.WriteLine("kabul edildi");
+            else { Console.WriteLine("kabul edilmedi"); }
+
+
+        }
+    }
+}
