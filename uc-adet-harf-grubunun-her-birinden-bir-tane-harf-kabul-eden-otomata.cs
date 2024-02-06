35bb23868988af2a3f626ccf6210227fa03b1055
esraakrkmz <109651527+esraakrkmz@users.noreply.github.com>
2024-02-07 00:26:33 +0300

    uc-adet-harf-grubunun-her-birinden-bir-tane-harf-kabul-eden-otomata

---
+++ uc-adet-harf-grubunun-her-birinden-bir-tane-harf-kabul-eden-otomata.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace otoörnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //abc , def , ghi şekilinde üç adet harf grubumuz bulunsun bunların içinden sadece birer harf kabul
+            //eden otomata kodu
+            string s = "adh";
+            int[,] q = new int[4,10];
+            int state = 0;
+            q[0, 0] = 1;
+            q[0, 1] = 1;
+            q[0, 2] = 1;
+            for (int i = 3; i < 9; i++)
+            {
+                q[0, i] = 3;
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                q[1, i] = 3;
+            }
+            q[1,3] = 2 ;
+            q[1,4] = 2;
+            q[1, 5] = 2;
+
+            q[2, 6] = 0;
+            q[2, 7] = 0;
+            q[2, 8] = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                q[2, i] = 3;
+            }
+
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                q[3, i] = 3;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                state = q[state, s[i] - 'a'];
+            }
+            if (state==3)
+            {
+                Console.WriteLine("bulundu");
+            }
+           else
+            {
+                Console.WriteLine("bulunmadı");
+            }
+
+        }
+    }
+}
